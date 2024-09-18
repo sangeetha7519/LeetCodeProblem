d4af9dda3f1777bb9ae9599d5bfc9b25c0ad2674
sangeetha7519 <sangeethamoorthy7519@gmail.com>
2024-09-18 17:42:24 +0530

    Update reverse_.py

--- reverse_.py
+++ reverse_.py
@@ -1,4 +1,4 @@
-![Screenshot 2024-09-18 174053](https://github.com/user-attachments/assets/442e9da5-0512-41e8-be62-699e3f275271)
+
 class Solution:
     def reverse(self, x: int) -> int:
         INT_MIN, INT_MAX = -2**31, 2**31 - 1
